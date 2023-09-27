6fb54d73f7cf0f49d8d2fac58e50a62adbc83fd8
Gustavo Gutierrez <gustavogtznav@gmail.com>
2023-09-26 21:12:25 -0700

    Primera parte terminad

---
+++ tarea-05/regex.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define open-paren-regex ...)
+
+(define close-paren-regex ...)
+
+(define define-regex ...)
+
+(define sum-regex ...)
+
+(define mult-regex ...)
+
+(define identifier-regex ...)
+
+(define number-regex ...)
+
+(provide open-paren-regex
+         close-paren-regex
+         define-regex
+         sum-regex
+         mult-regex
+         identifier-regex
+         number-regex)
