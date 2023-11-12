c21900fd3e14429d58cba3b5e1577564c8aabdd5
Gustavo Gutierrez <gustavogtznav@gmail.com>
2023-11-12 14:09:59 -0700

    Ya se hizo la 4, falta arreglar el formato de como se imprimen

--- tarea-07/let-vals.rkt
+++ tarea-07/let-vals.rkt
@@ -17,7 +17,8 @@
      (error (format "Expected integer but got boolean ~a" x))]
     [(pair-val x)
      (error (format "Expected integer but got pair ~a" x))]
-    ))
+    [(null-val)
+     (error (format "Expected integer but got null ~a" x))]))
 
 (define (expval->bool x)
   (match x
@@ -25,15 +26,30 @@
     [(num-val x)
      (error (format "Expected boolean but got integer ~a" x))]
     [(pair-val x)
-     (error (format "Expected boolean but got pair ~a" x))]))
+     (error (format "Expected boolean but got pair ~a" x))]
+    [(null-val)
+     (error (format "Expected boolean but got null ~a" x))]))
 
 (define (expval->pair x)
-  (match x
+  (match 
     [(pair-val x) x]
     [(num-val x)
      (error (format "Expected pair but got integer ~a" x))]
     [(bool-val x)
-     (error (format "Expected pair but got boolean ~a" x))]))
+     (error (format "Expected pair but got boolean ~a" x))]
+    [(null-val)
+     (error (format "Expected pair but got null ~a" x))]))
+
+(define (expval->null x)
+  (match x
+    [(null-val) '()]
+    [(num-val x)
+     (error (format "Expected null but got integer ~a" x))]
+    [(bool-val x)
+     (error (format "Expected null but got boolean ~a" x))]
+    [(pair-val x)
+     (error (format "Expected null but got pair ~a" x))]))
+
 
 (provide
  expval?
@@ -41,6 +57,7 @@
   [expval->num (-> expval? integer?)]
   [expval->bool (-> expval? boolean?)]
   [expval->pair (-> expval? pair?)]
+  [expval->null (-> expval? null?)]
   [struct num-val ((x integer?))]
   [struct bool-val ((x boolean?))]
   [struct pair-val ((x pair?))]
